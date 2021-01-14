7ae55ac575e29df0196ff5dcb1a6efcb008fcce2
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-01-13 23:38:09 -0500

    yes

--- api/server.js
+++ api/server.js
@@ -1,13 +1,17 @@
 const User = require("./userData.js");
 const express = require("express");
 const server = express();
-server.use(express.json());
+// server.use(express.json());
+
+server.get('/', (req,res)=>{
+    res.send("hello world")
+})
 
 // GET all users
-server.get("api/users", async(req,res)=>{
-    try{
-        res.status(200).json(users);
-    } catch(err) {
-        res.status(500).json({error:err.message});
-    }
-})
+// server.get("api/users", async(req,res)=>{
+//     try{
+//         res.status(200).json(users);
+//     } catch(err) {
+//         res.status(500).json({error:err.message});
+//     }
+// })
