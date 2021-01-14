8241d92aae9216638934a9e94ef904d5965856d5
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-01-14 00:14:33 -0500

    all endpoints created

--- api/server.js
+++ api/server.js
@@ -36,4 +36,38 @@ server.post("/api/users", async (req, res) => {
     }
   });
 
+  server.delete("/api/users/:id", async(req,res)=>{
+      const {id}= req.params;
+      try {
+        const user = await User.delete(id);
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res.status(404).json({ message: "unknown ID" });
+        }
+      } catch (err) {
+        res.status(500).json({ error: err.message });
+      }
+  })
+
+  server.put('/api/users/:id', async (req,res)=> {
+    const {id} = req.params;
+    const changes = req.body;
+
+    if(!user.name || !dpg.weight){
+        res.status(400).json({message:'must include weight and name'})
+    } else {
+        try{
+            const updatedUser = await user.update(id,changes);
+            if(updatedUser){
+                res.status(200).json(updateUser);
+            }else{
+                res.status(404).json({message:"unkown id"});
+            }
+        }catch(err){
+            res.status(500).json({error:err.message})
+        }
+    }
+})
+
 module.exports = server;
